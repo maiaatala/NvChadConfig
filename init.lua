dfb4742356558a10d9c914f0dd6d139ce1dca280
maiaatala <ana.atala@unemat.br>
2023-08-12 15:17:14 -0400

    feat(nvimTree): open nvim tree on startup, changing to folder if buffer is a folder

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
 -- vim.opt.colorcolumn = "80"
+local function open_nvim_tree(data)
+  -- is buffer a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if directory then
+    -- change to the directory
+    vim.cmd.cd(data.file)
+  end
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
