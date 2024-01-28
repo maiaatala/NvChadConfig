a5cba4a9655d3832889f7ee3bd15fa5240e3165e
maiaatala <ana.atala@unemat.br>
2024-01-28 00:16:43 -0400

    refactor: lsp eslintd

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -17,7 +17,11 @@ local sources = {
 
   null_ls.builtins.code_actions.eslint_d, -- suggest fixes from eslint
 
-  null_ls.builtins.diagnostics.eslint_d, --show error message on the screen
+  null_ls.builtins.diagnostics.eslint_d.with {
+    condition = function(utils)
+      return utils.root_has_file { ".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs" }
+    end,
+  }, --show error message on the screen
 }
 
 null_ls.setup {
