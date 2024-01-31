aa9e61ded76cd9506574ccffd916c8ef6a0a60f4
maiaatala <ana.atala@unemat.br>
2024-01-31 01:17:30 -0400

    map: add mapping to keep cursor in middle while searching

--- mappings.lua
+++ mappings.lua
@@ -8,9 +8,11 @@ M.abc = {
   n = {
     ["<C-d>"] = { "<C-d>zz", "N:Keep cursor middle when moving paga down" },
     ["<C-u>"] = { "<C-u>zz", "N:Keep cursor middle when moving paga up" },
+    ["<n>"] = { "nzzzv", "N:Keep cursor middle when searching" },
+    ["<N>"] = { "Nzzzv", "N:Keep cursor middle when searching" },
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>v"] = { ":vsp", "split screen vertically", opts = { nowait = true } },
-    ["<leader>h"] = { ":sp", "split screen horizontally", opts = { nowait = true } },
+    ["<leaer>h"] = { ":sp", "split screen horizontally", opts = { nowait = true } },
     ["<leader>tt"] = {
       function()
         require("base46").toggle_transparency()
