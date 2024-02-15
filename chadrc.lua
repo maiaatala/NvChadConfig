9c262dc70d288a4dbb6c569347265cf6b967f9f1
maiaatala <ana.atala@unemat.br>
2024-02-15 18:20:45 -0400

    style: remove unnecessary buttons and change statusline

--- chadrc.lua
+++ chadrc.lua
@@ -2,7 +2,20 @@ local highlights = require "custom.configs.highlights_overrides"
 
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = "monekai", transparency = true, hl_add = highlights.add }
+M.ui = {
+  theme = "palenight",
+  transparency = true,
+  hl_add = highlights.add,
+  statusline = {
+    theme = "default",
+    separator_style = "arrow",
+  },
+  tabufline = {
+    overriden_modules = function(modules)
+      table.remove(modules, 4)
+    end,
+  },
+}
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
