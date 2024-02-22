e59dc84f2813539b4a688b88814f9fc5c8d67580
maiaatala <ana.atala@unemat.br>
2024-02-22 18:30:55 -0400

    style: remove git branch status and place ai status on it

--- chadrc.lua
+++ chadrc.lua
@@ -10,15 +10,17 @@ M.ui = {
     theme = "default",
     separator_style = "arrow",
     overriden_modules = function(modules)
+      table.remove(modules, 3) -- remove git branch info
+
       table.insert(
         modules,
-        7,
+        3,
         (function()
           return (
             "%#St_codeiumStatus#"
-            .. "󰘦 "
+            .. " 󰘦 "
             .. vim.api.nvim_call_function("codeium#GetStatusString", {})
-            .. " ~ "
+            .. " ~"
           )
         end)()
       )
