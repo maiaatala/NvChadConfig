5ee507d48534534954a4d045fa1f6e21564f35e7
maiaatala <ana.atala@unemat.br>
2024-04-04 07:53:14 -0400

    style(wip): remove visual noise

--- chadrc.lua
+++ chadrc.lua
@@ -3,32 +3,37 @@ local highlights = require "custom.configs.highlights_overrides"
 ---@type ChadrcConfig
 local M = {}
 M.ui = {
-  theme = "palenight",
+  theme = "catppuccin",
   transparency = true,
   hl_add = highlights.add,
+  hl_override = highlights.override,
   statusline = {
-    theme = "default",
+    theme = "vscode_colored",
     separator_style = "arrow",
     overriden_modules = function(modules)
+      table.remove(modules, 13) -- remove folder
+      table.remove(modules, 11) -- remove file type
+      table.remove(modules, 10) -- remove file encoding
+      table.remove(modules, 9) -- remove cursor position
       table.remove(modules, 3) -- remove git branch info
 
-      table.insert(
-        modules,
-        3,
-        (function()
-          return (
-            "%#St_codeiumStatus#"
-            .. " 󰘦 "
-            .. vim.api.nvim_call_function("codeium#GetStatusString", {})
-            .. " ~"
-          )
-        end)()
-      )
+      -- table.insert(
+      --   modules,
+      --   7,
+      --   (function()
+      --     return (
+      --       "%#St_codeiumStatus#"
+      --       .. "󰘦 "
+      --       .. vim.api.nvim_call_function("codeium#GetStatusString", {})
+      --       .. " ~ "
+      --     )
+      --   end)()
+      -- )
     end,
   },
   tabufline = {
     overriden_modules = function(modules)
-      table.remove(modules, 4) -- remove the buttons from upper left corner
+      table.remove(modules, 4) -- remove the buttons from upper right corner
     end,
   },
 }
