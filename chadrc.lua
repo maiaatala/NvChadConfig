89f7246060aa830dedb9abdd52fab373c90c28ce
maiaatala <ana.atala@unemat.br>
2024-06-06 22:04:46 -0400

    wip: testing statusline lsp message bug fix

--- chadrc.lua
+++ chadrc.lua
@@ -8,28 +8,28 @@ M.ui = {
   hl_add = highlights.add,
   hl_override = highlights.override,
   statusline = {
-    theme = "vscode_colored",
+    theme = "default",
     separator_style = "arrow",
     overriden_modules = function(modules)
-      table.remove(modules, 13) -- remove folder
-      table.remove(modules, 11) -- remove file type
-      table.remove(modules, 10) -- remove file encoding
-      table.remove(modules, 9) -- remove cursor position
-      table.remove(modules, 8) -- remove changes and additions
-      table.remove(modules, 3) -- remove git branch info
-
-      table.insert(
-        modules,
-        8,
-        (function()
-          return (
-            "%#St_codeiumStatus#"
-            .. "󰘦 "
-            .. vim.api.nvim_call_function("codeium#GetStatusString", {})
-            .. " ~ "
-          )
-        end)()
-      )
+      -- table.remove(modules, 13) -- remove folder
+      -- table.remove(modules, 11) -- remove file type
+      -- table.remove(modules, 10) -- remove file encoding
+      -- table.remove(modules, 9) -- remove cursor position
+      -- table.remove(modules, 8) -- remove changes and additions
+      -- table.remove(modules, 3) -- remove git branch info
+      --
+      -- table.insert(
+      --   modules,
+      --   8,
+      --   (function()
+      --     return (
+      --       "%#St_codeiumStatus#"
+      --       .. "󰘦 "
+      --       .. vim.api.nvim_call_function("codeium#GetStatusString", {})
+      --       .. " ~ "
+      --     )
+      --   end)()
+      -- )
     end,
   },
   tabufline = {
